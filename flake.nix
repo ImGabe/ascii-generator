7fcec523b768e7a867f4d528a1c4ab0f02479416
ImGabe <gabrielpmonte@hotmail.com>
2021-07-07 10:55:38 -0300

    update flake

--- flake.nix
+++ flake.nix
@@ -2,23 +2,83 @@
   description = "Ascii-generator flake";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    crate2nix = {
+      url = "github:kolloch/crate2nix";
+      flake = false;
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, utils, ... }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      rec {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            cargo
+  outputs = { self, nixpkgs, utils, rust-overlay, crate2nix, ... }:
+    let
+      name = "ascii-generator";
+    in
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          # Imports
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              rust-overlay.overlay
+              (self: super: {
+                rustc = self.rust-bin.stable.latest.default;
+                cargo = self.rust-bin.stable.latest.default;
+              })
+            ];
+          };
+          inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
+            generatedCargoNix;
+
+          # Create the cargo2nix project
+          project = pkgs.callPackage
+            (generatedCargoNix {
+              inherit name;
+              src = ./.;
+            })
+            {
+              defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+                ${name} = oldAttrs: {
+                  inherit nativeBuildInputs;
+                };
+              };
+            };
+
+          nativeBuildInputs = with pkgs; [
+            # rust
             rustc
-            rustfmt
+            cargo
             rust-analyzer
+
+            # tools
+            nixpkgs-fmt
           ];
-        };
-      });
+        in
+        rec {
+          packages.${name} = project.rootCrate.build;
+
+          # `nix build`
+          defaultPackage = packages.${name};
+
+          # `nix run`
+          apps.${name} = utils.lib.mkApp {
+            inherit name;
+            drv = packages.${name};
+          };
+          defaultApp = apps.${name};
+
+          # `nix develop`
+          devShell = pkgs.mkShell
+            {
+              inherit nativeBuildInputs;
+              RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+            };
+        }
+      );
 }
